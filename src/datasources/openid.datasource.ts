5997eb81cc0ec1afb89326b97947c866277d8541
Stuart Caunt <caunt@ill.fr>
2022-05-11 15:34:14 +0200

    Tests with performing client-side token validation without performing a request to the SSO every authentication call. This obtains the public key from the SSO at startup and decrypts the access token signature. Verifies that the issuer is correct too and that the client_id is coherent. Doesn't follow standard OpenID Connect flow as the ID token is not requested (however the required information appears to be in the access token anyway).

--- src/datasources/openid.datasource.ts
+++ src/datasources/openid.datasource.ts
@@ -1,12 +1,17 @@
 import { APPLICATION_CONFIG } from '../application-config';
 import { Client, Issuer, UserinfoResponse } from 'openid-client';
 import { AuthenticationError, logger } from '../utils';
+import { verify, decode } from 'jsonwebtoken';
+import JwksRsa, { JwksClient } from 'jwks-rsa';
 
 export class OpenIDDataSource  {
   static dataSourceName = 'open-id';
 
+  private _issuer: Issuer<Client>;
   private _client: Client;
   private _clientPromise: Promise<Client>;
+
+  private _signingKey: string;
  
   get client(): Promise<Client> {
     return (async () => {
@@ -45,16 +50,32 @@ export class OpenIDDataSource  {
 
   async authenticate(token: string): Promise<UserinfoResponse> {
     try {
-      const client = await this.client;
-      const userInfo = await client.userinfo(token);
+
+      const jwt = verify(token, this._signingKey, {
+        issuer: this._issuer.issuer as string,
+        complete: true
+      });
+
+      // Check if client_id is passed in the token and confirm it matches what we expect
+      if (jwt.payload['azp'] && jwt.payload['azp'] !== APPLICATION_CONFIG().idp.clientId) {
+        throw new AuthenticationError('Token does not correspond to the client');
+      }
+
+      // const client = await this.client;
+      // const userInfo = await client.userinfo(token);
+      
+      const userInfo = jwt.payload as UserinfoResponse;
       return userInfo;
-    
+   
     } catch (error) {
-      if (error instanceof Error) {
-        throw new AuthenticationError(`Authentication error: ${error.message}`);
+      if (error instanceof AuthenticationError) {
+        throw error;
+        
+      } else if (error instanceof Error) {
+        throw new AuthenticationError(`${error.message}`);
 
       } else {
-        throw new AuthenticationError(`Authentication error: ${error}`);
+        throw new AuthenticationError(`${error}`);
       }
     }
   }
@@ -64,11 +85,16 @@ export class OpenIDDataSource  {
     const clientId = APPLICATION_CONFIG().idp.clientId;
 
     try {
-      logger.debug('Creating new OpenID connect client');
-      const issuer = await Issuer.discover(idpUrl);
-      const client = new issuer.Client({
-        client_id: clientId
+      logger.debug('Creating new OpenID Connect client');
+      this._issuer = await Issuer.discover(idpUrl);
+      const client = new this._issuer.Client({
+        client_id: clientId,
+        // userinfo_signed_response_alg: 'RS256',
       });
+
+
+      logger.debug('Obtaining signing key from OpenID Connect provider');
+      this._signingKey = await this.getSigningKey();
 
       return client;
     } catch (error) {
@@ -81,6 +107,29 @@ export class OpenIDDataSource  {
 
       process.exit();
     }
+  }
+
+  getSigningKey(): Promise<string> {
+    const metadata = this._issuer.metadata;
+    const jwksUri = metadata.jwks_uri;
+
+    return new Promise((resolve, reject) => {
+      const jwksClient = new JwksRsa.JwksClient({
+        jwksUri: jwksUri
+      });
+
+      jwksClient.getSigningKey(null, function(error, key: any) {
+        const signingKey = key.publicKey || key.rsaPublicKey;
+
+        if (error) {
+          logger.error(`Failed to get signing key from JWKS endpoint of the OpenID Connect provider ${jwksUri} : ${error.message}`);
+          reject(error);
+
+        } else {
+          resolve(signingKey);
+        }
+      });
+    });
   }
 
 }
